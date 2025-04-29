9e6b380017ae5d229ffd3beffc8e22528cdf44b7
MrRacoonGH <hadrien181107@gmail.com>
2025-04-29 17:11:08 +0200

    ok computer

--- js/app.js
+++ js/app.js
@@ -47,3 +47,78 @@ document.querySelectorAll('.select li').forEach(item => {
     });
   });
   
+
+
+
+  //non fonctionelle :
+
+
+
+  function removeCard(element) {
+    let card = element.closest(".card");
+    if (card) {
+        card.remove();
+    }
+}
+
+
+  fetch('../data/data.json')
+    .then(response => response.json())
+    .then(items => {
+        const $wrapper = document.querySelector(".items-wrapper");
+
+        items.forEach(function(item) {
+            const $card = document.createElement("div");
+            $card.classList.add("card");
+
+            const $info = document.createElement("div");
+            $info.classList.add("info");
+
+            const $image = document.createElement("img");
+            $image.src = item.logo;
+            $image.alt = item.name;
+
+            const $details = document.createElement("div");
+            $details.classList.add("details");
+
+            const $name = document.createElement("h2");
+            $name.textContent = item.name;
+
+            const $description = document.createElement("p");
+            $description.textContent = item.description;
+
+            const $action = document.createElement("div");
+            $action.classList.add("extensions-action");
+
+            const $removeItem = document.createElement("li");
+            $removeItem.textContent = "remove";
+
+            const $switchLabel = document.createElement("label");
+            $switchLabel.classList.add("switch");
+
+            const $toggleBtn = document.createElement("input");
+            $toggleBtn.type = "checkbox";
+            $toggleBtn.id = "toggleBtn";
+            $toggleBtn.checked = item.isActive;
+
+            const $slider = document.createElement("span");
+            $slider.classList.add("slider");
+
+            $switchLabel.appendChild($toggleBtn);
+            $switchLabel.appendChild($slider);
+
+            $info.appendChild($image);
+            $info.appendChild($details);
+            $details.appendChild($name);
+            $details.appendChild($description);
+
+            $action.appendChild($removeItem);
+            $action.appendChild($switchLabel);
+
+            $card.appendChild($info);
+            $card.appendChild($action);
+
+            $wrapper.appendChild($card);
+        });
+    })
+    .catch(error => console.error("Erreur lors du chargement du JSON :", error));
